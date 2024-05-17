b4ed2ef8d8b33c82324b717c2fc0328915cbb4c3
Farhod <wedsdsa22@gmail.com>
2024-05-17 10:02:05 -0700

    Added ExistingRooms.jsx and RoomFilter.jsx

---
+++ dev-hotel/src/components/room/ExistingRooms.jsx
@@ -0,0 +1,20 @@
+import React, {useState} from "react";
+
+
+const ExistingRooms = () => {
+  const [rooms, setRooms] = useState([])
+  const [currentPage, setCurrentPage] = useState(1)
+  const [roomsPerPage] = useState(8)
+  const [isLoading, setIsLoading] = useState(false)
+  const [filteredRooms, setFilteredRooms] = useState([])
+  const [selectedRoomType, setSelectedRoomType] = useState("")
+  const [successMessage, setSuccessMessage] = useState("")
+  const [errorMessage, setErrorMessage] = useState("")
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default ExistingRooms;
